a78fba3f9d6089bb2cf2c33431466bc0a3f61419
kelly <78395711+jincanke@users.noreply.github.com>
2025-02-06 18:23:04 +0800

    Update CellMapToSpot.R

--- R/CellMapToSpot.R
+++ R/CellMapToSpot.R
@@ -1,9 +1,9 @@
 #' Calculate the spatial variable genes
 #'
-#' @param sc_norm
-#' @param sc_meta
+#' @param sc_norm A gene * cell matrix
+#' @param sc_meta A dataframe of cells
 #' @param st_norm
-#' @param spatial_location
+#' @param spatial_location A dataframe of spots, column `HMRF_cluster` containing the spatial domain label.
 #'
 #' @return
 #' @export
@@ -31,74 +31,65 @@ map_spot_genes = function(sc_norm,
   return(st_gene_list)
 }
 
-
-#' Version_3: SSIM_Map
+#' Level 2 mapping (OptimalSpot), globally optimizing the assigned spots of cells within each domain
 #'
 #' @param sc_norm
 #' @param sc_meta
 #' @param st_norm
 #' @param spatial_location
-#' @param pred_sc_svm
-#' @param pred_st_svm
-#' @param num_epochs
-#' @param para_distance
-#' @param para_density
-#' @param batch
-#' @param python_path
-#' @param pca_method
+#' @param batch Whether do batch removal or not.
+#' @param st_gene_list The spatial gene lists of each domain
+#' @param pca_method Select the PCA function, support `prcomp_irlba` or `prcomp`.
+#' @param pred_sc_svm The vector storing the predicted domain label of cells and the vector name is the cell id
+#' @param pred_st_svm The vector storing the predicted domain label of spots and the vector name is the spot id
+#' @param python_path Provide the path of called python
+#' @param num_epochs The number of epochs
+#' @param para_distance The weight of expression pattern similarity
+#' @param para_density The weight of cellular spatial density
 #'
-#' @return
+#' @return A dataframe storing the corresponding relationship between cells and spots.
 #' @export
 #'
 #' @examples
-map_cell_to_spot = function(sc_norm,
-                            sc_meta,
-                            st_norm,
-                            spatial_location,
-                            batch = TRUE,
-                            pca_method = 'prcomp_irlba',
-                            pred_sc_svm,
-                            pred_st_svm,
-                            python_path,
-                            num_epochs=2000L,
-                            para_distance=1.0,
-                            para_density=1.0
-){
-
-  st_gene_list <- map_spot_genes(sc_norm = sc_norm,
-                                 sc_meta = sc_meta,
-                                 st_norm = st_norm,
-                                 spatial_location = spatial_location)
-  shvgs <- unique(unlist(st_gene_list))
-
-  # map cell to spot
+map_cell_to_spot <- function(sc_norm,
+                             sc_meta,
+                             st_norm,
+                             spatial_location,
+                             batch = TRUE,
+                             st_gene_list = NULL,
+                             pca_method = "prcomp_irlba",
+                             pred_sc_svm,
+                             pred_st_svm,
+                             python_path,
+                             num_epochs = 2000L,
+                             para_distance = 1.0,
+                             para_density = 1.0){
+  if(is.null(st_gene_list)){
+    st_gene_list <- map_spot_genes(sc_norm = sc_norm, sc_meta = sc_meta,
+                                   st_norm = st_norm, spatial_location = spatial_location)
+    shvgs <- unique(unlist(st_gene_list))
+  }
   cluster <- sort(unique(pred_sc_svm))
   cell_spot_map <- data.frame()
-
-  for(j in 1:length(cluster)){
+  for (j in 1:length(cluster)) {
     cluster_id = cluster[j]
-    single_cell_id <- names(pred_sc_svm[pred_sc_svm==cluster_id])
-    spot_id  <- names(pred_st_svm[pred_st_svm==cluster_id])
-    sub_embedding <- data_to_transform(sc_norm = sc_norm[,single_cell_id,drop=FALSE],
-                                       st_norm = st_norm[,spot_id,drop=FALSE],
+    single_cell_id <- names(pred_sc_svm[pred_sc_svm == cluster_id])
+    spot_id <- names(pred_st_svm[pred_st_svm == cluster_id])
+    sub_embedding <- data_to_transform(sc_norm = sc_norm[, single_cell_id, drop = FALSE],
+                                       st_norm = st_norm[, spot_id, drop = FALSE],
                                        spatial_genes = unique(st_gene_list[[j]]),
-                                       batch=batch,
-                                       pca_method=pca_method)
-    cellxgene <- as.data.frame(t(sub_embedding)[single_cell_id,,drop=FALSE])
-    spotxgene <- as.data.frame(t(sub_embedding)[spot_id,,drop=FALSE])
-
+                                       batch = batch,
+                                       pca_method = pca_method)
+    cellxgene <- as.data.frame(t(sub_embedding)[single_cell_id, , drop = FALSE])
+    spotxgene <- as.data.frame(t(sub_embedding)[spot_id, , drop = FALSE])
     reticulate::use_condaenv(python_path)
-    reticulate::py_run_string(paste0("import sys; sys.path.append('", system.file("python",package="CMAP"), "')"))
-    reticulate::source_python(paste(system.file("python",package="CMAP"),"mapping01.py",sep='/'))
-    cell_spot <- map_cell(cellxgene,spotxgene,num_epochs,para_distance,para_density)
-    cell_spot_map_sub <- data.frame(Single_cell=rownames(cellxgene),
-                                    Spot=rownames(spotxgene)[apply(cell_spot,1,which.max)],
-                                    Probability=apply(cell_spot,1,max))
-    cell_spot_map <- rbind(cell_spot_map,cell_spot_map_sub)
+    reticulate::py_run_string(paste0("import sys; sys.path.append('", system.file("python", package = "CMAP"), "')"))
+    reticulate::source_python(paste(system.file("python", package = "CMAP"), "mapping01.py", sep = "/"))
+    cell_spot <- map_cell(cellxgene, spotxgene, num_epochs, para_distance, para_density)
+    cell_spot_map_sub <- data.frame(Single_cell = rownames(cellxgene),
+                                    Spot = rownames(spotxgene)[apply(cell_spot, 1, which.max)],
+                                    Probability = apply(cell_spot, 1, max))
+    cell_spot_map <- rbind(cell_spot_map, cell_spot_map_sub)
   }
-
   return(cell_spot_map)
 }
-
-
-
